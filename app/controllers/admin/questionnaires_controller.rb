dc0717741773006c2e6f9c23e0595201636552c5
air_wang <air@whit.ah.cn>
2016-04-08 10:42:44 +0800

    优化csv导出数据接口

--- app/controllers/admin/questionnaires_controller.rb
+++ app/controllers/admin/questionnaires_controller.rb
@@ -3,7 +3,13 @@ class Admin::QuestionnairesController < Admin::BaseController
     @questionnaires = Questionnaire.order(id: :desc).page params[:page]
     respond_to do |format|
       format.html
-      format.csv {send_data Questionnaire.to_csv, filename: "result.csv"}
+      format.csv {
+        if Questionnaire.count > 0
+          send_data "\xEF\xBB\xBF" << Questionnaire.to_csv, filename: "result.csv"
+        else
+          send_data "\xEF\xBB\xBF" << "当前没有投票数据！", filename: "result.csv"
+        end
+        }
     end
   end
 end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -31,10 +31,17 @@ class Questionnaire < ActiveRecord::Base
   scope :where_any, ->(key, value) { where("data @> hstore(?, ?)", key, value) }
 
   def self.to_csv(options = {})
-    CSV.generate(options) do |csv|
-      csv << first.data.keys
-      all.each do |survey|
-        csv << survey.data.values
+    output = CSV.generate(options) do |csv|
+      if last.present?
+        basic_info_header = new.attributes.keys - ["id", "ip", "data", "created_at", "updated_at"]
+        header = ["ID", "IP", "投票时间"] + last.data.keys + basic_info_header
+        csv <<  header
+        all.each do |survey|
+          content = [survey.id, survey.ip, survey.created_at] + survey.data.values + [survey.gender, survey.grade, survey.local, survey.hope,
+                  survey.learn_status, survey.edu, survey.income, survey.school,
+                  survey.position, survey.school_position, survey.known]
+          csv << content
+        end
       end
     end
   end
